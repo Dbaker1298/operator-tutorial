238caf47793b0dc2e4a524b01dfe44c43295b08d
David Baker <dpbaker1298@gmail.com>
2025-03-01 14:51:27 -0500

    add logging to the reconcile function

--- internal/controller/ghost_controller.go
+++ internal/controller/ghost_controller.go
@@ -47,7 +47,9 @@ type GhostReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.20.2/pkg/reconcile
 func (r *GhostReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx)
+	log.Info("Reconciling Ghost")
+	log.Info("Reconciliation complete")
 
 	// TODO(user): your logic here
 
