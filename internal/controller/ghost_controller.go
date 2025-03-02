604ab047f58df7724507721aed00311839488ae3
David Baker <dpbaker1298@gmail.com>
2025-03-02 11:39:21 -0500

    Implement the Ghost Operator Logic, Part 4 - Service

--- internal/controller/ghost_controller.go
+++ internal/controller/ghost_controller.go
@@ -26,6 +26,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -55,6 +56,55 @@ const (
 // +kubebuilder:rbac:groups="",resources=persistentvolumeclaims,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;patch;delete
+
+func (r *GhostReconciler) addServiceIfNotExists(ctx context.Context, ghost *blogv1.Ghost) error {
+	log := log.FromContext(ctx)
+	service := &corev1.Service{}
+	err := r.Get(ctx, client.ObjectKey{Namespace: ghost.ObjectMeta.Namespace, Name: svcNamePrefix + ghost.ObjectMeta.Namespace}, service)
+	if err != nil && client.IgnoreNotFound(err) != nil {
+		return err
+	}
+
+	if err == nil {
+		// Service exists
+		return nil
+	}
+	// Service does not exist, create it
+	desiredService := generateDesiredService(ghost)
+	if err := controllerutil.SetControllerReference(ghost, desiredService, r.Scheme); err != nil {
+		return err
+	}
+
+	// Service does not exist, create it
+	if err := r.Create(ctx, desiredService); err != nil {
+		return err
+	}
+	r.recoder.Event(ghost, corev1.EventTypeNormal, "ServiceCreated", "Service created successfully")
+	log.Info("Service created", "service", desiredService.Name)
+	return nil
+}
+
+func generateDesiredService(ghost *blogv1.Ghost) *corev1.Service {
+	return &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "ghost-service-" + ghost.ObjectMeta.Namespace,
+			Namespace: ghost.ObjectMeta.Namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			Type: corev1.ServiceTypeNodePort,
+			Ports: []corev1.ServicePort{
+				{
+					Port:       80,
+					TargetPort: intstr.FromInt(2368),
+					NodePort:   30001,
+				},
+			},
+			Selector: map[string]string{
+				"app": "ghost-" + ghost.ObjectMeta.Namespace,
+			},
+		},
+	}
+}
 
 func (r *GhostReconciler) addOrUpdateDeployment(ctx context.Context, ghost *blogv1.Ghost) error {
 	log := log.FromContext(ctx)
